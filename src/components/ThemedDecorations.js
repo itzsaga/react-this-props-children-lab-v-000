fd5d7ec8dbece5dd2e0dd107aa849b337e24eca4
Seth Alexander <seth.alexander@wework.com>
2018-03-10 01:08:44 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,18 +1,1 @@
-import React from 'react'
-
-class ThemedDecorations extends React.Component {
-  render() {
-    const addedPropChildren = React.Children.map(this.props.children, child => {
-      return React.cloneElement(child, {
-        className: this.props.theme
-      })
-    })
-    return (
-      <div>
-        {addedPropChildren}
-      </div>
-    )
-  }
-}
-
-export default ThemedDecorations
+// Code ThemedDecoration Component Here
